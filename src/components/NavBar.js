0f09a2c82664b6f4308d7d6470fd6174a968c3d4
Connall Gallagher <shapeboss@live.ie>
2024-07-02 19:45:25 +0100

    Added NavBar elements and added logout feature

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,47 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import cal_image from "../assets/cal_image.jpg";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import axios from "axios";
 
 const NavBar = () => {
-  const currentUser = useCurrentUser()
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  const addTask = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/tasks/create"
+    >
+      <i className="far fa-plus-square"></i>Add task
+    </NavLink>
+  );
+  const loggedInIcons = <>
+      <NavLink
+      className={styles.NavLink}
+      to={`/profiles/${currentUser?.profile_id}`}
+    >
+      <i className="far fa-plus-square"></i>Profile
+    </NavLink>
+    <NavLink
+      className={styles.NavLink}
+      to="/"
+      onClick = {handleSignOut}
+    >
+      <i className="fas fa-sign-out-alt"></i>Sign out
+    </NavLink>
+
+  </>;
   const loggedOutIcons = (
     <>
       <NavLink
@@ -36,7 +71,7 @@ const NavBar = () => {
             <img src={cal_image} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
-
+        {currentUser && addTask}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
