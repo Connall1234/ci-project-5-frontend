2df90bc31336b5b3a2058052b52c052fc1f2b328
Connall Gallagher <shapeboss@live.ie>
2024-08-04 15:51:39 +0100

    Styling Sign up form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,29 +1,21 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
-import styles from "../../styles/SignInUpForm.module.css";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+import styles from "../../styles/SignUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-import axios from "axios";
-
-import {
-  Form,
-  Button,
-  Image,
-  Col,
-  Row,
-  Container,
-  Alert,
-} from "react-bootstrap";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const SignUpForm = () => {
   const [signUpData, setSignUpData] = useState({
+    first_name: '',
+    last_name: '',
     username: '',
     password1: '',
     password2: '',
   });
-  const { username, password1, password2 } = signUpData;
+
+  const { first_name, last_name, username, password1, password2 } = signUpData;
   const [errors, setErrors] = useState({});
   const history = useHistory();
 
@@ -45,89 +37,115 @@ const SignUpForm = () => {
   };
 
   return (
-    <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign up</h1>
-
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="username">
-              <Form.Label className="d-none">Username</Form.Label>
-              <Form.Control
-                className={styles.Input}
-                type="text"
-                placeholder="username"
-                name="username"
-                value={username}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.username?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
-            <Form.Group controlId="password1">
-              <Form.Label className="d-none">Password</Form.Label>
-              <Form.Control
-                className={styles.Input}
-                type="password"
-                placeholder="password"
-                name="password1"
-                value={password1}
-                onChange={handleChange}
-              />
+    <Container fluid className="p-0">
+      <Row className="min-vh-100 no-gutters align-items-center">
+        <Col md={6} className={`d-flex align-items-center justify-content-center ${styles.FormCol}`}>
+          <Container className={`${appStyles.Content} p-4`}>
+            <h1 className={styles.Header}>Sign Up</h1>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="first_name">
+                <Form.Label className="sr-only">First Name</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type="text"
+                  placeholder="First Name"
+                  name="first_name"
+                  value={first_name}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId="last_name">
+                <Form.Label className="sr-only">Last Name</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type="text"
+                  placeholder="Last Name"
+                  name="last_name"
+                  value={last_name}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId="username">
+                <Form.Label className="sr-only">Username</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                  value={username}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
+              {errors.username?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
+              <Form.Group controlId="password1">
+                <Form.Label className="sr-only">Password</Form.Label>
+                <Form.Control
+                  className={styles.Input}
+                  type="password"
+                  placeholder="Password"
+                  name="password1"
+                  value={password1}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
               {errors.password1?.map((message, idx) => (
                 <Alert variant="warning" key={idx}>
                   {message}
                 </Alert>
               ))}
               <Form.Group controlId="password2">
-                <Form.Label className="d-none">Confirm password</Form.Label>
+                <Form.Label className="sr-only">Confirm Password</Form.Label>
                 <Form.Control
                   className={styles.Input}
                   type="password"
-                  placeholder="Confirm password"
+                  placeholder="Confirm Password"
                   name="password2"
                   value={password2}
                   onChange={handleChange}
+                  required
                 />
-                {errors.password2?.map((message, idx) => (
-                  <Alert variant="warning" key={idx}>
-                    {message}
-                  </Alert>
-                ))}
               </Form.Group>
-            </Form.Group>
-            <Button
-              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
-              type="submit"
-            >
-              Sign Up
-            </Button>
-            {errors.non_field_errors?.map((message, idx) => (
-              <Alert key={idx} variant="warning" className="mt-3">
-                {message}
-              </Alert>
-            ))}
-          </Form>
-        </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signin">
-            Already have an account? <span>Sign In</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={"https://logowik.com/content/uploads/images/calendar5662.jpg"}
-        />
-      </Col>
-    </Row>
+              {errors.password2?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
+              <Button
+                className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright} w-100`}
+                type="submit"
+              >
+                Sign Up
+              </Button>
+              {errors.non_field_errors?.map((message, idx) => (
+                <Alert key={idx} variant="warning" className="mt-3">
+                  {message}
+                </Alert>
+              ))}
+            </Form>
+          </Container>
+          <Container className={`mt-3 ${appStyles.Content}`}>
+            <Link className={styles.Link} to="/signin">
+              Already have an account? <span>Sign In</span>
+            </Link>
+          </Container>
+        </Col>
+        <Col md={6} className={`d-none d-md-flex align-items-center justify-content-center ${styles.ImageCol}`}>
+          <Image
+            className={`${appStyles.FillerImage} img-fluid`}
+            src="https://logowik.com/content/uploads/images/calendar5662.jpg"
+            alt="Calendar"
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
