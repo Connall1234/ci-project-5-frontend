d6f7434d51b6a43fde476bfd77e68cc35d241391
Connall Gallagher <shapeboss@live.ie>
2024-08-22 16:22:58 +0100

    Changed babel and config overr for deployment

--- config-overrides.js
+++ config-overrides.js
@@ -12,3 +12,14 @@
 //     return config;
 //   }
 // );
+
+const { override, addBabelPlugins } = require('customize-cra');
+
+module.exports = override(
+  ...addBabelPlugins(
+    "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-private-property-in-object",
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-private-methods"
+  )
+);
